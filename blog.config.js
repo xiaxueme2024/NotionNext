56d6790dd08b0138feadcae0e8b2b8bbec189fa8
xiaxueme2024 <154415690+xiaxueme2024@users.noreply.github.com>
2023-12-21 16:28:30 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '公众号爆款案例库', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '人人写文章10万➕', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://zimeiti.fun', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '公众号爆款案例库, 公众号, 订阅号, 10W阅读量爆款, 公众号上推荐, 订阅号上推荐, 订阅号运营, 公众号运营, 公众号运营变现, 订阅号运营变现, 订阅号无粉丝流量主收益过百, 公众号无粉丝流量主收益过百', // 网站关键词 英文逗号隔开
 
